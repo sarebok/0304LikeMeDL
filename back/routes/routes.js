56ed86361543a84d3e64e651b0686505f8d886d3
sareebok <bortonae@hotmail.com>
2024-03-09 03:22:36 -0300

    add getPost controller

--- back/routes/routes.js
+++ back/routes/routes.js
@@ -1,11 +1,9 @@
 import  express from "express";
+import { getPosts } from "../src/controllers/controllers.js";
 
 const router = express.Router();
 
 // GET route
-router.get('/posts', (req, res) => {
-    // Your code here
-    res.send('chao world desde routes')
-});
+router.get('/posts', getPosts);
 
 export default router;
---
+++ back/src/controllers/controllers.js
@@ -0,0 +1,6 @@
+const getPosts = async (req, res) => {
+    // Your code here
+    res.send('chao world desde controllers')
+}
+
+export { getPosts }
